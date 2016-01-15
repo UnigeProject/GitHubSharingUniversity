01f3c54c4907511fc6cf871c7cca2ff81e913442
Tia93 <parrinello.matti@gmail.com>
2016-01-15 18:05:24 +0100

    Commit

--- app/src/main/java/com/example/tia/githubsharingproject/MainActivity.java
+++ app/src/main/java/com/example/tia/githubsharingproject/MainActivity.java
@@ -2,20 +2,23 @@ package com.example.tia.githubsharingproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bottoneCheNascondere = (Button) findViewById(R.id.BottoneCheNasconde);
+    Button bottoneNascosto =(Button) findViewById(R.id.BottoneCheSiNasconde);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
-    public void provadiupdate (){
-        int i = 0;
-
-        int ii = 2;
-
-        int iii = 3;
+    public void onClickNascondino(){
+        bottoneNascosto.setVisibility(View.GONE);
     }
 }
